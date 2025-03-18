3c7f046846d4f1304ccd847619f9f3c47f2ea794
Sudhir Rathod <rathodsudhir326@gmail.com>
2025-03-18 18:30:09 +0530

    Fix: Allow Render host in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // Ensure React plugin is included
+  plugins: [react()],
   server: {
     port: process.env.PORT || 5173,
     host: "0.0.0.0",
+    strictPort: true,
+    allowedHosts: ["react-weather-app-1-ftts.onrender.com"], // Add this line
   },
 });
