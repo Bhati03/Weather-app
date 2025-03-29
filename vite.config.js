64d396039eee035e6f0b24b4c05dea36db56bc01
Bhati03 <140142366+Bhati03@users.noreply.github.com>
2025-03-29 11:55:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
     port: process.env.PORT || 5173,
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: ["react-weather-app-1-ftts.onrender.com"], // Add this line
+    allowedHosts: ["weather-app-xybc.onrender.com"], // Add this line
   },
 });
